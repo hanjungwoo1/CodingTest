bbcef8b4ce933a4c172a1d13ee9ebb97fbe098e4
jungwoo <hanjungwoo1@naver.com>
2022-01-21 16:11:59 +0900

    change dir

---
+++ 이것이 취업을 위한 코딩 테스트다/Chapter10/10-1.py
@@ -0,0 +1,55 @@
+# 기본적인 서로소 집합 알고리즘
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+"""
+입력예시
+6 4
+"""
+
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v+1) # 부모 테이블 초기화
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v+1):
+    parent[i] = i
+
+"""
+입력예시
+1 4
+2 3
+2 4
+5 6
+"""
+
+# union 연산을 각자 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+# 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합: ', end = '')
+for i in range(1, v+1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+# 부모 테이블 내용 출력
+print('부모 테이블: ', end = '')
+for i in range(1, v+1):
+    print(parent[i], end=' ')
