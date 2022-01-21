95b24c3fdf3b1baae845574d30309ee1d7dc6c6f
jungwoo <hanjungwoo1@naver.com>
2022-01-21 16:32:52 +0900

    programmers level 1

---
+++ programmers/Level 1/신고결과받기.py
@@ -0,0 +1,32 @@
+def solution(id_list, report, k):
+    user = {}   # 신고 횟수 딕셔너리
+    name = {}   # 신고 결과 딕셔너리
+
+    # 딕셔너리 초기화, for count
+    for id in id_list:
+        user[id] = 0
+        name[id] = 0
+
+    # report map으로 중복제거
+    set_report = set(report)
+    report = list(set_report)
+
+    # 신고 당한 횟수 계산
+    for data in report:
+        _, second = data.split(' ')
+        user[second] += 1
+
+    # 각각 자신이 신고한 사람이 k를 넘었는지 판별, 신고 결과 만들기
+    for data in report:
+        first, second = data.split(' ')
+        if user[second] >= k:
+            name[first] += 1
+
+    # 정답
+    answer = []
+
+    # for dictionary for loop
+    for k, v in name.items():
+        answer.append(v)
+
+    return answer
---
+++ programmers/README.md
@@ -0,0 +1,3 @@
+# 프로그래머스 코딩 연습  by jungwoo
+
+## Python3
