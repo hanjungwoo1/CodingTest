21782f783319e1a8e849e2295851d3e7eb035b6e
jungwoo <hanjungwoo1@naver.com>
2022-01-22 23:40:58 +0900

    이것이 코딩 테스트다 chapter 3

---
+++ 이것이 취업을 위한 코딩 테스트다/Chapter3/단순하게 푸는 답안 예시.py
@@ -0,0 +1,23 @@
+# N, M, K를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True:
+    for i in range(k): # 가장 큰 수를 k 번 더하기
+        if m == 0: # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1 # 더할 때마다 1씩 빼기
+    if m == 0: # m이 0이라면 반복문 탈출
+        break
+    result += second # 두 번째로 큰 수를 한 번 더하기
+    m -= 1 # 더할 때마다 1씩 빼기
+
+print(result) # 최종 답안 출력
