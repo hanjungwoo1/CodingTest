ab4ab287c161fae5a338b658bf4ec439058564c7
jungwoo <hanjungwoo1@naver.com>
2022-03-20 23:59:22 +0900

    coding test

--- Backjoon/test2.py
+++ Backjoon/test2.py
@@ -1,5 +1,25 @@
-N = int(input())
+rows = 4
+columns = 3
 
-for _ in range(N):
-    a, b = map(int, input().split())
-    print(a + b)
+
+
+data = []
+num = 1
+for i in range(rows):
+    now = []
+    for j in range(columns):
+        now.append(num)
+        num += 1
+    data.append(now)
+
+print(data)
+
+x1, y1, x2, y2 = 2, 1,2, 3
+
+temp = []
+
+for x in range(x1-1, x2):
+    print(x)
+    temp.append(data[x][y2-1])
+
+print(temp)
