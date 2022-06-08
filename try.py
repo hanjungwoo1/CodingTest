9d104c252d6a86542883d27a4983395727a06011
jungwoo <hanjungwoo1@naver.com>
2022-06-08 21:52:06 +0900

    coding test

---
+++ try.py
@@ -0,0 +1,18 @@
+import time
+import pyautogui
+
+ID = ["hanjungwoo1", "hanjungwoo2"]
+password = ["1234", "1235"]
+
+
+
+for x,y in zip(ID,password):
+    print(x, y)
+
+
+
+while True:
+    time.sleep(2)
+    print(pyautogui.position())
+
+
